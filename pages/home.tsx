e7651067051674e7909f11be1946748f3d358294
Paul Makles <paulmakles@gmail.com>
2019-06-15 13:10:15 +0100

    Actually commit the files

---
+++ pages/home.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import { PageType } from '../src/Page';
+import { withTranslation } from '../src/i18n'
+
+class Home extends React.Component<PageType> {
+	render() {
+		const translate = this.props.t;
+    	return (
+			<div>{translate`string.welcome`}</div>
+    	)
+  	}
+}
+
+export default withTranslation('common')(Home)
---
+++ src/i18n.ts
@@ -0,0 +1,31 @@
+import i18next from 'i18next';
+import NextI18Next from 'next-i18next'
+
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const NextI18NextInstance = new NextI18Next({
+	defaultLanguage: 'en-GB',
+	otherLanguages: ['en-US', 'cs-CZ'],
+	browserLanguageDetection: true
+})
+
+export default NextI18NextInstance;
+export const {
+	appWithTranslation,
+	withTranslation,
+} = NextI18NextInstance;
+
+i18next
+	.use(NextI18NextInstance)
+	.use(LanguageDetector)
+	.init({
+		fallbackLng: 'en-GB',
+		debug: true,
+		interpolation: {
+			escapeValue: false
+		}
+	});
+
+initReactI18next
+	.init(i18next);

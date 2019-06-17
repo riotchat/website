6534da8be97c63630c2f34d54338e0d7a40f1e3c
Martin Löffler <me@fatalerrorcoded.eu>
2019-06-17 22:18:34 +0200

    Authorize page Username tweaks
    Co-authored-by: nizune <49092977+nizune@users.noreply.github.com>

--- pages/authorize.tsx
+++ pages/authorize.tsx
@@ -18,7 +18,7 @@ class Authorize extends App {
 	render() {
     	return (
 			<div>
-				<Header title={translate`homepage.name`} />
+				<Header title="Authorize Application" />
 				<main>
                     <div style={{background: "darkred", height: "100vh"}}>
                         <Navigation fixed={true} />
@@ -26,13 +26,13 @@ class Authorize extends App {
                             <div className={css['header-info']}>
                                 <div className={css.text}>
                                     <p>Connect your YouTube account</p>
-                                    <span style={{ fontSize: "40px", fontWeight: "bold" }}>Username</span>
+                                    <span className={cssAuth.at}>@</span>
+                                    <span className={cssAuth.username}>Username</span>
+                                    <span className={cssAuth.tag}>#0000</span>
                                     <p>Integrate YouTube with Riot to get access to features like video sharing, a "Now Playing" status and synced video playback.</p>
                                 </div>
                                 <div className={css.buttons}>
-                                    {/* <a id="download" href="riot.html" class="button" style="background-color: mediumslateblue"><i class='bx bxl-windows'></i> Download</a> */}
-
-                                    <a href="#sign-up" className={cssAuth.btnAuth}><i className={`${css.icon} bx bxl-youtube`} style={{color: 'red'}}></i>Authorize YouTube</a>
+                                    <a href="#sign-up" className={cssAuth.btnAuth}>Authorize YouTube</a>
                                     <a href="#lightweight" className={cssAuth.btnAuthCancel}>Cancel Authorization</a>
                                 </div>
                             </div>

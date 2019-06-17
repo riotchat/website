88b0b34f80f134ffaf94d662df8b1cf0acf3268f
Paul Makles <paulmakles@gmail.com>
2019-06-17 20:42:44 +0100

    Import 404 page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { translate, withTranslation } from '../src/i18n';
 
+import Header from '../src/components/Header';
+import { Footer, Navigation } from '../src/components/structure/HeaderFooter';
+import { Trans } from 'react-i18next';
+
 class ErrorPage extends React.Component<{statusCode: number}> {
 	static getInitialProps({ res, err }) {
 		const statusCode = res ? res.statusCode : err ? err.statusCode : null
@@ -8,15 +12,29 @@ class ErrorPage extends React.Component<{statusCode: number}> {
 	}
 
 	render() {
-		if (this.props.statusCode === 404) {
-    		return (
-				<div>404: {translate`error.404`}</div>
-			)
-		} else {
-			return (
-				<div>Caught error: {this.props.statusCode}</div>
-			);
-		}
+		return (
+			<div>
+				<Header title={'Pro'} />
+				{
+					this.props.statusCode === 404 ?
+						<main>
+							<Navigation />
+							<div style={{ textAlign: "center" }}>
+								<img src="/static/images/404.png" width="100%" style={{ maxWidth: "600px" }} />
+								<h2>{translate`error.404`}</h2>
+								<p><Trans i18nKey='error.goback'>
+									<a href="/"></a>
+								</Trans></p>
+							</div>
+						</main>
+					:
+						<main>
+							<div>Caught error: {this.props.statusCode}</div>
+						</main>
+				}
+				<Footer />
+			</div>
+		);
   	}
 }
 
